4860b3b71e3c76771e67542d756de5264ecd5555
卢小玉 <923121067@qq.com>
2016-11-22 19:45:02 +0800

    在tools工具包写了 存放所有接口的 UrlValues 类

--- app/src/main/java/com/lu/beauty/base/BaseFragment.java
+++ app/src/main/java/com/lu/beauty/base/BaseFragment.java
@@ -1,8 +1,8 @@
 package com.lu.beauty.base;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
  * Fragment基类@wqs
  */
 
-public abstract class BaseFragment extends Fragment{
+public abstract class BaseFragment extends Fragment {
 
     @Nullable
     @Override
---
+++ app/src/main/java/com/lu/beauty/tools/UrlValues.java
@@ -0,0 +1,34 @@
+package com.lu.beauty.tools;
+
+/**
+ * Created by XiaoyuLu on 16/11/22.
+ */
+
+public class UrlValues {
+    // 画报 接口
+    // 第一级 和 第二级 接口
+    public static final String ARTICLE_URL = "http://design.zuimeia.com/api/v1/articles/daily/simple/?page=1&page_size=30";
+    public static final String ARTICLE_DETAIL_URL = "http://design.zuimeia.com/api/v1/article/112/";
+
+    // 有物 接口
+    // 物品分类的接口
+    public static final String PRODUCT_CATEGORIES_URL = "http://design.zuimeia.com/api/v1/product/categories/";
+    // Daily项第一级
+    public static final String PRODUCT_DAILY_URL = "http://design.zuimeia.com/api/v1/products/daily/";
+    // 普通第一级 和 第二级 接口
+    public static final String PRODUCT_COMMON_URL = "http://design.zuimeia.com/api/v1/products/category/3/?page=1&page_size=30";
+    public static final String PRODUCT_DETAIL_URL = "http://design.zuimeia.com/api/v1/product/1559/";
+
+    // 设计师 接口
+    // 设计师分类
+    public static final String DESIGNER_CATEGORIES_URL = "http://design.zuimeia.com/api/v1/designer/categories/";
+    // 推荐 第一级
+    public static final String DESIGNER_RECOMMEND_URL = "http://design.zuimeia.com/api/v1/designers/recommend/?page=1&page_size=30";
+    // 独立设计师 第一级
+    public static final String DESIGNER_INDEPENDENT_URL = "http://design.zuimeia.com/api/v1/designers/category/30/?page=1&page_size=30";
+    // 大牌设计师 第一级
+    public static final String DESIGNER_BIG_URL = "http://design.zuimeia.com/api/v1/designers/category/31/?page=1&page_size=30";
+    // 第二级
+    public static final String DESIGNER_DETAIL_URL = "http://design.zuimeia.com/api/v1/designer/145/";
+
+}
