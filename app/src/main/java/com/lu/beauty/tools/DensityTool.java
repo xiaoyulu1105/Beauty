e89a6e6ee5676239ac224315c0187866e6fa1477
zhouyunxiao <625047296@qq.com>
2016-11-24 21:38:19 +0800

    笑脸和哭脸的自定义View

---
+++ app/src/main/java/com/lu/beauty/tools/DensityTool.java
@@ -0,0 +1,27 @@
+package com.lu.beauty.tools;
+
+import android.content.Context;
+
+/**
+ * Created by  AngleXiao on 16/11/24.
+ * OLiGei  what is your name
+ * 轻松拿下一个类 属实有牌面
+ */
+
+public class DensityTool {
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
