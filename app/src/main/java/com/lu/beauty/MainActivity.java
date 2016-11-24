aa82eb1264a5fdd4b63257ea788284724e128bf0
zhouyunxiao <625047296@qq.com>
2016-11-24 08:49:48 +0800

    空

--- app/src/main/java/com/lu/beauty/MainActivity.java
+++ app/src/main/java/com/lu/beauty/MainActivity.java
@@ -2,13 +2,17 @@ package com.lu.beauty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 小玉创建了项目
+
     }
 }
