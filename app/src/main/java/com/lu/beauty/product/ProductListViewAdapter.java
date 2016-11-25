5c684ca820d5e13ed79aa4ec3c981d3467241301
王桥生 <15942807925@163.com>
2016-11-25 16:24:48 +0800

    no message

--- app/src/main/java/com/lu/beauty/product/ProductListViewAdapter.java
+++ app/src/main/java/com/lu/beauty/product/ProductListViewAdapter.java
@@ -16,20 +16,20 @@ import java.util.ArrayList;
  */
 
 public class ProductListViewAdapter extends BaseAdapter{
-    ArrayList<ProductDailyBean> arrayList;
+    ProductDailyBean bean;
 
-    public void setArrayList(ArrayList<ProductDailyBean> arrayList) {
-        this.arrayList = arrayList;
+    public void setBean(ProductDailyBean bean) {
+        this.bean = bean;
     }
 
     @Override
     public int getCount() {
-        return arrayList == null ? 0 : arrayList.size();
+        return bean.getData().getProducts().size();
     }
 
     @Override
     public Object getItem(int position) {
-        return arrayList.get(position);
+        return bean.getData().getProducts().get(position);
     }
 
     @Override
@@ -41,12 +41,12 @@ public class ProductListViewAdapter extends BaseAdapter{
     public View getView(int position, View convertView, ViewGroup parent) {
 
         CommonViewHolder viewHolder = CommonViewHolder.getViewHolder(convertView, parent, R.layout.item_product_daily);
-        viewHolder.setText(R.id.daily_item_title, arrayList.get(position).getData().getProducts().get(position).getName())
-                .setText(R.id.daily_item_name, arrayList.get(position).getData().getProducts().get(position).getDesigner().getName())
-                .setText(R.id.daily_item_label, arrayList.get(position).getData().getProducts().get(position).getDesigner().getLabel())
-                .setImage(R.id.daily_item_img, arrayList.get(position).getData().getProducts()
+        viewHolder.setText(R.id.daily_item_title, bean.getData().getProducts().get(position).getName())
+                .setText(R.id.daily_item_name, bean.getData().getProducts().get(position).getDesigner().getName())
+                .setText(R.id.daily_item_label, bean.getData().getProducts().get(position).getDesigner().getLabel())
+                .setImage(R.id.daily_item_img, bean.getData().getProducts()
                         .get(position).getCover_images().get(0), parent.getContext())
-                .setCircleImage(R.id.daily_item_user_icon, arrayList.get(position).getData()
+                .setCircleImage(R.id.daily_item_user_icon, bean.getData()
                         .getProducts().get(position).getDesigner().getAvatar_url(), parent.getContext());
 
 
