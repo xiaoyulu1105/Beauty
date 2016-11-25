d3224b45bdfbd7c54c4c43215f1df48764bea96a
卢小玉 <923121067@qq.com>
2016-11-25 08:55:50 +0800

    ...

--- app/src/main/java/com/lu/beauty/base/MyApp.java
+++ app/src/main/java/com/lu/beauty/base/MyApp.java
@@ -1,8 +1,23 @@
 package com.lu.beauty.base;
 
+import android.app.Application;
+import android.content.Context;
+
 /**
  * Created by XiaoyuLu on 16/11/25.
  */
 
-public class MyApp {
+public class MyApp extends Application {
+
+    private static Context sContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sContext = this;
+    }
+
+    public static Context getContext(){
+        return sContext;
+    }
 }
--- app/src/main/java/com/lu/beauty/product/GetPercent.java
+++ app/src/main/java/com/lu/beauty/product/GetPercent.java
@@ -2,7 +2,41 @@ package com.lu.beauty.product;
 
 /**
  * Created by XiaoyuLu on 16/11/25.
+ *
+ * 获得喜欢 和 不喜欢的百分比 和 高度
  */
 
 public class GetPercent {
+
+    private static double maxHeight; // 最高的高度, 你们想最高为多少, 就通过set方法设置多少
+
+    public void setMaxHeight(double maxHeight) {
+        this.maxHeight = maxHeight;
+    }
+
+    public static double getMaxHeight() {
+        return maxHeight;
+    }
+
+    public static double getLikeHigh(double likeNum, double uLikeNum) {
+        double likeHigh = (likeNum / (likeNum + uLikeNum)) * maxHeight;
+        return likeHigh;
+    }
+
+    public static double getNoLikeCount(double likeNum, double uLikeNum) {
+        double noLike = (uLikeNum / (likeNum + uLikeNum)) * 100;
+
+        return noLike;
+    }
+
+    public static double getNoLikeHigh(double likeNum, double uLikeNum) {
+        double noLikeHigh = (uLikeNum / (likeNum + uLikeNum)) * maxHeight;
+        return noLikeHigh;
+    }
+
+    public static double getLikeCount(double likeNum, double uLikeNum) {
+        double LikeCount = (likeNum / (likeNum + uLikeNum)) * 100;
+
+        return LikeCount;
+    }
 }
