3ecfc73997cda55bb7da0ec627a900e6aae5bca7
郭轩宇 <1013355265@qq.com>
2016-12-01 21:25:51 +0800

    no message

---
+++ app/src/main/java/com/lu/beauty/bean/DesignerHeadlineBean.java
@@ -0,0 +1,8 @@
+package com.lu.beauty.bean;
+
+/**
+ * Created by GuoXuanYu on 16/11/30.
+ */
+
+public class DesignerHeadlineBean {
+}
---
+++ app/src/main/java/com/lu/beauty/designer/PopAdapter.java
@@ -0,0 +1,8 @@
+package com.lu.beauty.designer;
+
+/**
+ * Created by GuoXuanYu on 16/11/30.
+ */
+
+public class PopAdapter {
+}
