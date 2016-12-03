9f2be42bf271856c91560a200488fb58240da4b4
卢小玉 <923121067@qq.com>
2016-12-03 12:13:24 +0800

    画报二级实现侧滑退出, Top的头像和Author的头像实现圆形显示.

--- app/src/main/java/com/lu/beauty/article/ArticleDetailActivity.java
+++ app/src/main/java/com/lu/beauty/article/ArticleDetailActivity.java
@@ -1,6 +1,10 @@
 package com.lu.beauty.article;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,18 +14,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.lu.beauty.R;
 import com.lu.beauty.base.BaseActivity;
 import com.lu.beauty.bean.ArticleDetailBean;
 import com.lu.beauty.internet.HttpUtil;
 import com.lu.beauty.internet.ResponseCallBack;
 import com.lu.beauty.richtext.HtmlTextView;
+import com.lu.beauty.tools.CircleDrawable;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.Utils;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
 
 /**
  * Created by XiaoyuLu on 16/11/29.
  * 显示 画报详细信息 的Activity
+ * 实现了侧滑退出
  */
-public class ArticleDetailActivity extends BaseActivity implements View.OnClickListener {
+public class ArticleDetailActivity extends BaseActivity implements View.OnClickListener, SwipeBackActivityBase {
 
 
     private int mGetId;
@@ -30,14 +43,18 @@ public class ArticleDetailActivity extends BaseActivity implements View.OnClickL
     private Button mTopReturnBtn;   // 返回
     private TextView mTopUsernameTV; // 显示的设计师名字
     private TextView mTopWhereTV;   // 设计师来自的地方
-    private ImageView mTopAuthorIconIV; // 设计师头像
-    private HtmlTextView mHtmlTextView;
-    private TextView mTitleTV;
+    private ImageView mTopDesignerIconIV; // 设计师头像
+
+    private TextView mTitleTV; // title相关的组件
     private TextView mSubTitleTV;
     private ImageView mTitleImageIV;
-    private ImageView mAuthorIconIV;
+
+    private ImageView mAuthorIconIV; // 编辑者相关的组件
     private TextView mAuthorNameTV;
     private TextView mAuthorSignTV;
+
+    private HtmlTextView mHtmlTextView;
+    private SwipeBackActivityHelper swipeBackActivityHelper; // 侧滑退出所用
 
 
     @Override
@@ -52,7 +69,7 @@ public class ArticleDetailActivity extends BaseActivity implements View.OnClickL
         mTopReturnBtn = bindView(R.id.article_detail_top_return_btn);
         mTopUsernameTV = bindView(R.id.article_detail_top_username_tv);
         mTopWhereTV = bindView(R.id.article_detail_top_where_tv);
-        mTopAuthorIconIV = bindView(R.id.article_detail_top_icon_img);
+        mTopDesignerIconIV = bindView(R.id.article_detail_top_icon_img);
 
         // 界面开始 与Title相关 的组件, 标题, 副标题, 图片
         mTitleTV = bindView(R.id.article_detail_title_title_tv);
@@ -76,8 +93,32 @@ public class ArticleDetailActivity extends BaseActivity implements View.OnClickL
         Intent intent = getIntent();
         mGetId = intent.getIntExtra(ArticleFragment.INTENT_ID_KEY, 117);
 
+        // 使用OkHTTP 请求画报的二级数据
         articleDetailDataRequest();
+
+        // 实现侧滑 需要绑定 activity
+        swipeBackActivityHelper = new SwipeBackActivityHelper(this);
+        swipeBackActivityHelper.onActivityCreate();
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.article_detail_top_return_btn:
+                finish();
+
+                break;
+            case R.id.article_detail_title_image_iv:
+                Toast.makeText(this, "点击了图片", Toast.LENGTH_SHORT).show();
+                Log.d("ArticleDetailActivity", "点击了图片");
+
+                break;
+            default:
+                Log.d("ArticleDetailActivity", "点击出错啦!");
+                break;
+        }
+    }
+
 
     private void articleDetailDataRequest() {
         HttpUtil.getArticleDetailBean(mGetId, new ResponseCallBack<ArticleDetailBean>() {
@@ -102,54 +143,125 @@ public class ArticleDetailActivity extends BaseActivity implements View.OnClickL
      * @param articleDetailBean 网络请求的数据
      */
     private void showHttpData(ArticleDetailBean articleDetailBean) {
+
         ArticleDetailBean.DataBean dataBean = articleDetailBean.getData();
-        // 获取 设计师的简单信息: 名字, 城市, 头像
-        String designerName = dataBean.getDesigners().get(0).getName();
-        String designerCity = dataBean.getDesigners().get(0).getCity();
-        String designerIcon = dataBean.getDesigners().get(0).getAvatar_url();
-        // 获得 大标题, 副标题, 图片
-        String title = dataBean.getTitle();
-        String subTitle = dataBean.getSub_title();
-        String image_url = dataBean.getImage_url();
+
+        // 显示 顶端的数据 的方法
+        showTopData(dataBean);
+        // 显示 Title相关的数据 的方法
+        showTitleData(dataBean);
+        // 显示 编辑者相关的数据 的方法
+        showAuthorData(dataBean);
+
+        // 获得 富文本数据
+        String content = dataBean.getContent();
+        // 显示 富文本数据
+        mHtmlTextView.setHtmlFromString(content);
+
+
+
+    }
+
+    /**
+     * 显示画报二级 author编辑者相关 的数据
+     * @param dataBean 传递过来的数据: articleDetailBean.getData()
+     */
+    private void showAuthorData(ArticleDetailBean.DataBean dataBean) {
         // 获得 编辑者的信息: 头像, 名字, 标签sign
         String authorIcon = dataBean.getAuthor().getAvatar_url();
         String authorName = dataBean.getAuthor().getUsername();
         String authorSign = dataBean.getAuthor().getSign();
-        // 获得 富文本数据
-        String content = dataBean.getContent();
 
-        // 显示 富文本数据
-        mHtmlTextView.setHtmlFromString(content);
+        // 显示 作者信息
+        mAuthorNameTV.setText(authorName);
+        mAuthorSignTV.setText(authorSign);
+//        Glide.with(ArticleDetailActivity.this).load(authorIcon).into(mAuthorIconIV);
+        Glide.with(ArticleDetailActivity.this).load(authorIcon).asBitmap().into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                CircleDrawable drawable = new CircleDrawable(resource);
+                mAuthorIconIV.setImageDrawable(drawable);
+            }
+        });
+    }
+
+    /**
+     * 显示画报二级 title相关 的数据
+     * @param dataBean 传递过来的数据: articleDetailBean.getData()
+     */
+    private void showTitleData(ArticleDetailBean.DataBean dataBean) {
+        // 获得 大标题, 副标题, 图片
+        String title = dataBean.getTitle();
+        String subTitle = dataBean.getSub_title();
+        String image_url = dataBean.getImage_url();
+
         // 显示 标题信息
         mTitleTV.setText(title);
         mSubTitleTV.setText(subTitle);
         Glide.with(ArticleDetailActivity.this).load(image_url).into(mTitleImageIV);
-        // 显示 作者信息
-        Glide.with(ArticleDetailActivity.this).load(authorIcon).into(mAuthorIconIV);
-        mAuthorNameTV.setText(authorName);
-        mAuthorSignTV.setText(authorSign);
+
+    }
+
+    /**
+     * 显示画报二级顶端Top 的数据
+     * @param dataBean 传递过来的数据: articleDetailBean.getData()
+     */
+    private void showTopData(ArticleDetailBean.DataBean dataBean) {
+        // 获取 设计师的简单信息: 名字, 城市, 头像
+        String designerName = dataBean.getDesigners().get(0).getName();
+        String designerCity = dataBean.getDesigners().get(0).getCity();
+        String designerIcon = dataBean.getDesigners().get(0).getAvatar_url();
+
         // 显示 top 栏设计师的信息
         mTopUsernameTV.setText(designerName);
         mTopWhereTV.setText(designerCity);
-        Glide.with(ArticleDetailActivity.this).load(designerIcon).into(mTopAuthorIconIV);
+//        Glide.with(ArticleDetailActivity.this).load(designerIcon).into(mTopDesignerIconIV);
+        Glide.with(ArticleDetailActivity.this).load(designerIcon).asBitmap().into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                CircleDrawable drawable = new CircleDrawable(resource);
+                mTopDesignerIconIV.setImageDrawable(drawable);
+            }
+        });
     }
 
 
+    /**
+     * 复写 SwipeBackActivityBase接口的三个抽象方法:
+     * getSwipeBackLayout, setSwipeBackEnable, scrollToFinishActivity.
+     * 此外还需要手动复写另外两个方法: onPostCreate, findViewById
+     */
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.article_detail_top_return_btn:
-                finish();
+    public SwipeBackLayout getSwipeBackLayout() {
+        // 修改了返回值
+        return swipeBackActivityHelper.getSwipeBackLayout();
+    }
 
-                break;
-            case R.id.article_detail_title_image_iv:
-                Toast.makeText(this, "点击了图片", Toast.LENGTH_SHORT).show();
-                Log.d("ArticleDetailActivity", "点击了图片");
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        // 添加一行代码
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
 
-                break;
-            default:
-                Log.d("ArticleDetailActivity", "点击出错啦!");
-                break;
+    @Override
+    public void scrollToFinishActivity() {
+        Utils.convertActivityToTranslucent(this);
+        getSwipeBackLayout().scrollToFinishActivity();
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        swipeBackActivityHelper.onPostCreate();
+    }
+
+    @Override
+    public View findViewById(@IdRes int id) {
+        View v = super.findViewById(id);
+        if (v == null && swipeBackActivityHelper != null) {
+            return swipeBackActivityHelper.findViewById(id);
         }
+
+        return v;
     }
 }
